2a4e45c0b3330b544156cc475d88d65d3f4bed8f
Gianni GIUDICE <gianni.giudice@lacatholille.fr>
2021-06-16 18:07:22 +0200

    :sparkles: Question en cours

--- src/component/Question.js
+++ src/component/Question.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const Question = () => {
-    return <div className="bg-success">Hello world</div>
+    const [question, setQuestion] = useState();
+    const [answer, setAnswer] = useState();
+    const [history, setHistory] = useState([]);
+
+    useEffect(() => {
+        // Récupération de la première question
+        fetch('https://127.0.0.1:8000/api/questions/1').then(response => response.json()).then(data => console.log(data));
+    }, []);
+
+    useEffect(() => {
+        // Dès qu'il y a une réponse, on enregistre tout dans history à la suite, et on demande une nouvelle question
+        setHistory((prevState) => ({
+            ...prevState,
+            [question]: [answer]
+        }));
+        fetch('https://127.0.0.1:8000/api/questions/2').then(response => response.json()).then(data => console.log(data));
+    }, [answer]);
+
+    // Afficher dans le return la question et les options de réponse
+    return <div>
+
+    </div>
 }
 
 export default Question;
